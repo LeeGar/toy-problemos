0ebee3d43e03b1a1e1f64100b0c549e4f15b0aa2
Gar Lee <gardarr@gmail.com>
2016-05-12 13:55:03 -0700

    Completed coin change

--- coinChange.py
+++ coinChange.py
@@ -1,33 +1,44 @@
  # Given coins of different denominations, and a total amount of money
  # Write a function that computes the fewest # of coins needed for that amount
  # Assume there's an infinite number of the coins
+
+#Optimal sub solution --> C(Total) = min[i] { C(total-coins[i]), C(total-coins[i+1]), C(total-coins[n])} + 1
+
 import math
 
-def coinChange (coins, total):
-	fewest = -1
-	seen = {}
+class Solution(object):
+    def coinChange(self, coins_available, coins_used, amount):
+        """
+        :type coins_available: List[int]
+        :type coins_used: List[int]
+        :type amount: int
+        :rtype: int
+        """
 
-	coins.sort(reverse=True)
+        #If the sum of all coins used = target amount, return the list of coins used
+        if sum(coins_used) == amount: 
+        	yield coins_used
+        #If the sum of all coins used is greater than the target, we fucked up, pass
+        elif sum(coins_used) > amount:
+        	pass
+        #If there are no more available coins, then pass as well
+        elif len(coins_available) == 0:
+        	pass
+        else:
+        	for coin in self.coinChange(coins_available[:], coins_used+[coins_available[0]], amount):
+        		yield coin
+        	for coin in self.coinChange(coins_available[1:], coins_used, amount):
+        		yield coin
 
-	for i, coin in enumerate(coins):
-		if coin > total:
-			return fewest
-		if hasattr(seen, str(coin)):
-			pass
-		else:
-			seen[coin] = coin
 
-	for i, coin in enumerate(coins):
-		subTotal = total % coin
-		largeCoins = total / coin
 
-		if subTotal in coins:
-			fewest += 1
-		print largeCoins
 
-	print fewest + math.floor(largeCoins)
+x = Solution()
 
-	return fewest + math.floor(largeCoins)
-	
+solutions = [s for s in x.coinChange([1,2,3], [], 5)]
+for s in solutions:
+	print s
 
-coinChange([1,2,5], 11)
+print 'optimal solution: ', min(solutions, key=len)
+
+
